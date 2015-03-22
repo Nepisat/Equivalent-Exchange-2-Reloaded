0584d2cf031b9b7a985a5ca94c8d3b02d0981e41
Nepisat <nepisat>
2015-03-22 16:50:01 +0900

    New EMCMapper and ToolTip

--- src/main/ee2/nepisat/ee2/event/ToolTipEvent.java
+++ src/main/ee2/nepisat/ee2/event/ToolTipEvent.java
@@ -10,24 +10,20 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 public class ToolTipEvent {
 	@ForgeSubscribe(priority = EventPriority.LOWEST)
     public void addToolTip(ItemTooltipEvent event){
-		/*
+		
     	ItemStack stack = event.itemStack;
     	if(!stack.hasTagCompound()){
-			for (int i = 0; i<173; i++){
-				if (stack.itemID == BlockEMCMapper.is[i].itemID){
-					if (BlockEMCMapper.is[i].hasTagCompound()){
+					
 						if(stack.stackSize <= 1){
-							event.toolTip.add(EnumChatFormatting.GREEN + "EMC: "+EnumChatFormatting.WHITE+BlockEMCMapper.loadEMC(BlockEMCMapper.is[i]));
+							event.toolTip.add(EnumChatFormatting.GREEN + "EMC: "+EnumChatFormatting.WHITE+(BlockEMCMapper.getEmc(stack)));
 						}else{
-							event.toolTip.add(EnumChatFormatting.GREEN + "EMC: "+EnumChatFormatting.WHITE+BlockEMCMapper.loadEMC(BlockEMCMapper.is[i]));
-							event.toolTip.add(EnumChatFormatting.YELLOW + "スタックEMC: "+EnumChatFormatting.WHITE+BlockEMCMapper.loadEMC(BlockEMCMapper.is[i])*stack.stackSize);
+							event.toolTip.add(EnumChatFormatting.GREEN + "EMC: "+EnumChatFormatting.WHITE+BlockEMCMapper.getEmc(stack));
+							event.toolTip.add(EnumChatFormatting.YELLOW + "スタックEMC: "+EnumChatFormatting.WHITE+BlockEMCMapper.getEmc(stack)*stack.stackSize);
 						}
 						
-					}
+					
 				}
 			}
-    	}
-    	*/
-	}
-	
+    	
+		
 }
